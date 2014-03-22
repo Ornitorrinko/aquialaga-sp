fc6e1a6768bafe5786b018e2c33b645cb04f4a68
Vinícius <vinicius.ribeiro.carneiro@gmail.com>
2014-03-22 11:08:32 -0300

    adsffds

--- node/repositorio/CETOcorrencia.js
+++ node/repositorio/CETOcorrencia.js
@@ -3,7 +3,7 @@ var CETOcorrencia = function(sequelize, DataTypes){
 	return sequelize.define('CETOcorrencia', {
 		  	id  : 		{type: DataTypes.INTEGER, allowNull : false }
 			, endereco:	{type: DataTypes.STRING, allowNull : false }
-			, numero: 	{type: DataTypes.INTEGER, allowNull : false }
+			, numero: 	{type: DataTypes.STRING, allowNull : false }
 			, latitude:  {type: DataTypes.INTEGER, allowNull : false }
 			, longitude: {type: DataTypes.INTEGER, allowNull : false }
 			, quantidade: {type: DataTypes.INTEGER, allowNull : false }		  
