174fa105bde2b9b291a742198dd1959f821eec47
Vinícius <vinicius.ribeiro.carneiro@gmail.com>
2014-03-22 17:13:52 -0300

    king host

--- node/repositorios/index.js
+++ node/repositorios/index.js
@@ -1,12 +1,26 @@
+var mysql = require('mysql');
+
+
 var config = require('../config')
 
 module.exports.createConnection = function () {
-	var mysql      = require('mysql');
 	var connection = mysql.createConnection({ host : config.db.host
 		                                    , user :config.db.user
 		                                    , password : config.db.password
 											, port : config.db.port
 											, database : config.db.instance
 											});
+	
+	connection.config.queryFormat = function (query, values) {
+	  if (!values) return query;
+	  return query.replace(/\:(\w+)/g, function (txt, key) {
+	    if (values.hasOwnProperty(key)) {
+	      return this.escape(values[key]);
+	    }
+	    return txt;
+	  }.bind(this));
+	};
+
 	return connection
 }
+
